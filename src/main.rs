27c3bc8642a1ecde9be4ad4a384bc1592f7e9c98
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-22 14:37:32 +0200

    Basic data structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let mut w = Word::new("asdfg");
+    w.reveal('a');
+    println!("{} {:?}", w.word, w.revealed_chars);
+}
+
+struct Word {
+    word: String,
+    revealed_chars: Vec<char>,
+}
+
+impl Word {
+    fn new(word: &str) -> Self {
+        Word {
+            word: word.to_string(),
+            revealed_chars: Vec::<char>::new(),
+        }
+    }
+
+    fn reveal(&mut self, c: char) {
+        self.revealed_chars.push(c);
+    }
 }
